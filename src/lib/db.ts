2784b7adc8443dfe60f7d09c29d44e29d6a96225
engmohammedsalah1255-lgtm <engmohammedsalah1255-lgtm@users.noreply.github.com>
2026-09-05 15:39:19 +0200

    Use Neon HTTP adapter on Workers

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaNeonHTTP } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
@@ -6,7 +6,7 @@ const databaseUrl = process.env.DATABASE_URL ?? '';
 const isPostgres = /^(postgres|postgresql):\/\//i.test(databaseUrl);
 
 const prisma = isPostgres
-	? new PrismaClient({ adapter: new PrismaNeon({ connectionString: databaseUrl }) })
+	? new PrismaClient({ adapter: new PrismaNeonHTTP(databaseUrl, {}) })
 	: new PrismaClient();
 
 export const db = globalForPrisma.prisma ?? prisma;
